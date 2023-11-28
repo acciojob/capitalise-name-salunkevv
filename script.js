bbee68c43bfdbd493233100b9bbc5748462efca9
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-28 23:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let item= document.getElementById("fname");
+item.addEventListener("keyup",(e)=>
+{
+    if(e.key=="Enter")
+    {
+        item.value= item.value.toUpperCase();
+    }
+})
